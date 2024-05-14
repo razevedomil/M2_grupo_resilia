0bbdd2ba154f8094b75a2dbf00445d115c571769
Marlon-Dantas <92075470+Marlon-Dantas@users.noreply.github.com>
2024-05-14 00:53:22 -0300

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE inserir_dados()
+CREATE OR REPLACE PROCEDURE resilia.inserir_dados()
 LANGUAGE plpgsql
 AS $$
 BEGIN
@@ -166,5 +166,70 @@ BEGIN
 	SELECT nome, descricao, carga_horaria FROM temp_modulos;
 
 
+END;
+$$;
+
+
+CREATE OR REPLACE PROCEDURE resilia.criar_tabelas_resilia()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    -- Criando a tabela Cursos no esquema "resilia"
+    CREATE TABLE IF NOT EXISTS resilia.Cursos (
+        id_curso SERIAL PRIMARY KEY,
+        nome VARCHAR(100),
+        descricao TEXT,
+        carga_horaria_total INTEGER
+    );
+
+    -- Criando a tabela Módulos no esquema "resilia"
+    CREATE TABLE IF NOT EXISTS resilia.Modulos (
+        id_modulo SERIAL PRIMARY KEY,
+        nome VARCHAR(100),
+        descricao TEXT,
+        carga_horaria INTEGER,
+        id_curso INTEGER REFERENCES resilia.Cursos(id_curso)
+    );
+
+    -- Criando a tabela Turmas no esquema "resilia"
+    CREATE TABLE IF NOT EXISTS resilia.Turmas (
+        id_turma SERIAL PRIMARY KEY,
+        nome VARCHAR(100),
+        data_inicio DATE,
+        data_termino DATE,
+        id_curso INTEGER REFERENCES resilia.Cursos(id_curso)
+    );
+
+    -- Criando a tabela Alunos no esquema "resilia"
+    CREATE TABLE IF NOT EXISTS resilia.Alunos (
+        id_aluno SERIAL PRIMARY KEY,
+        nome VARCHAR(100),
+        email VARCHAR(100),
+        data_nascimento DATE,
+        telefone VARCHAR(20),
+        status VARCHAR(20)
+    );
+
+    -- Criando a tabela Facilitadores no esquema "resilia"
+    CREATE TABLE IF NOT EXISTS resilia.Facilitadores (
+        id_facilitador SERIAL PRIMARY KEY,
+        nome VARCHAR(100),
+        email VARCHAR(100),
+        telefone VARCHAR(20)
+    );
+
+    -- Criando tabela de relacionamento Alunos_Turmas no esquema "resilia"
+    CREATE TABLE IF NOT EXISTS resilia.Alunos_Turmas (
+        id_aluno INTEGER REFERENCES resilia.Alunos(id_aluno),
+        id_turma INTEGER REFERENCES resilia.Turmas(id_turma),
+        PRIMARY KEY (id_aluno, id_turma)
+    );
+
+    -- Criando tabela de relacionamento Facilitadores_Turmas no esquema "resilia"
+    CREATE TABLE IF NOT EXISTS resilia.Facilitadores_Turmas (
+        id_facilitador INTEGER REFERENCES resilia.Facilitadores(id_facilitador),
+        id_turma INTEGER REFERENCES resilia.Turmas(id_turma),
+        PRIMARY KEY (id_facilitador, id_turma)
+    );
 END;
 $$;
