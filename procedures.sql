5522e32340d418d2bbc6da2330c16c84d6c3cc03
Marlon-Dantas <92075470+Marlon-Dantas@users.noreply.github.com>
2024-05-14 00:40:30 -0300

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -1,16 +1,44 @@
---POCEDURE PARA INSERIR ALUNOS:
+CREATE OR REPLACE PROCEDURE inserir_dados()
+LANGUAGE plpgsql
+AS $$
 BEGIN
 
-	CREATE TEMP TABLE temp_alunos (
+
+	--INSERIR DADOS DO CURSO:
+	CREATE TEMP TABLE IF NOT EXISTS temp_cursos (
     nome varchar,
-    email varchar,
-    data_nascimento date,
-    telefone varchar,
-    status varchar
+    descricao varchar,
+    carga_horaria_total int
 );
 
-INSERT INTO temp_Alunos (nome, email, data_nascimento, telefone, status)
-VALUES	('João Silva', 'joao@email.com', '2000-01-15', '(11) 9999-8888', 'Ativo'),
+	INSERT INTO temp_cursos (nome, descricao, carga_horaria_total)
+	VALUES
+		('Desenvolvimento Web', 'Curso completo de desenvolvimento web', 180),
+		('Data Science', 'Curso abrangente de ciência de dados', 200),
+		('Design Gráfico', 'Curso prático de design gráfico', 150),
+		('Marketing Digital', 'Curso especializado em marketing online', 160),
+		('Gestão de Projetos', 'Curso de gerenciamento de projetos', 140),
+		('Inteligência Artificial', 'Curso avançado de IA', 220),
+		('Redes de Computadores', 'Curso sobre redes e conectividade', 190),
+		('Programação em Python', 'Curso prático de Python', 120),
+		('Banco de Dados', 'Curso completo de bancos de dados', 170),
+		('Segurança da Informação', 'Curso essencial de segurança cibernética', 200);
+
+	INSERT INTO resilia.Cursos (nome, descricao, carga_horaria_total)
+	SELECT nome, descricao, carga_horaria_total FROM temp_cursos;
+
+	--INSERIR DADOS DOS ALUNOS
+    CREATE TEMP TABLE IF NOT EXISTS temp_alunos (
+        nome varchar,
+        email varchar,
+        data_nascimento date,
+        telefone varchar,
+        status varchar
+    );
+
+    INSERT INTO temp_alunos (nome, email, data_nascimento, telefone, status)
+    VALUES
+		('João Silva', 'joao@email.com', '2000-01-15', '(11) 9999-8888', 'Ativo'),
 		('João Silva', 'joao@email.com', '2000-01-15', '(11) 9999-8888', 'Ativo'),
 		('Maria Santos', 'maria@email.com', '1998-05-20', '(21) 9876-5432', 'Ativo'),
 		('Pedro Oliveira', 'pedro@email.com', '1999-03-10', '(31) 1234-5678', 'Ativo'),
@@ -60,22 +88,20 @@ VALUES	('João Silva', 'joao@email.com', '2000-01-15', '(11) 9999-8888', 'Ativo'
 		('Rafael Souza', 'rafael_souza@email.com', '1998-01-10', '(82) 8888-9999', 'Ativo'),
 		('Sandra Fernandes', 'sandra@email.com', '1981-05-12', '(92) 9999-0000', 'Ativo'),
 		('Thiago Oliveira', 'thiago@email.com', '1992-06-08', '(02) 0000-1111', 'Ativo');
-		
-    INSERT INTO resilia.Alunos (nome, email, data_nascimento, telefone, status)
+
+    INSERT INTO resilia.alunos (nome, email, data_nascimento, telefone, status)
     SELECT nome, email, data_nascimento, telefone, status FROM temp_alunos;
-END;
 
---PROCEDURE PARA INSERIR FACILITADORES:
-BEGIN
-    -- Cria a tabela temporária
-    CREATE TEMP TABLE IF NOT EXISTS resilia.temp_facilitadores (
-        nome VARCHAR(255),
-        email VARCHAR(255),
-        telefone VARCHAR(20)
+
+	--- INSERIR FACILITADORES
+	
+	CREATE TEMP TABLE IF NOT EXISTS temp_facilitadores (
+        nome varchar,
+        email varchar,
+        telefone varchar
     );
-
-    -- Insere os dados na tabela temporária
-    INSERT INTO resilia.temp_facilitadores (nome, email, telefone)
+	
+	INSERT INTO temp_facilitadores (nome, email, telefone)
     VALUES
         ('Ana Silva', 'ana.silva@empresa.com', '(11) 9876-5432'),
         ('Pedro Santos', 'pedro.santos@empresa.com', '(21) 1234-5678'),
@@ -87,39 +113,58 @@ BEGIN
         ('Fernanda Souza', 'fernanda.souza@empresa.com', '(81) 7654-3210'),
         ('Diego Lima', 'diego.lima@empresa.com', '(91) 2345-6789'),
         ('Camila Ferreira', 'camila.ferreira@empresa.com', '(99) 8765-4321');
-
-    INSERT INTO resilia.facilitadores (nome, email, telefone)
+	
+	INSERT INTO resilia.facilitadores (nome, email, telefone)
     SELECT nome, email, telefone FROM temp_facilitadores;
-END;
+	
+	
+	---INSERIR DADOS NA TABELA TURMAS
+	CREATE TEMP TABLE IF NOT EXISTS temp_turmas (
+    nome varchar,
+    data_inicio date,
+    data_termino date
+);
+
+	INSERT INTO temp_turmas (nome, data_inicio, data_termino)
+	VALUES 
+		('Turma Desenvolvimento Web', '2024-07-01', '2024-08-31'),
+		('Turma Data Science', '2024-08-01', '2024-09-30'),
+		('Turma Design Gráfico', '2024-09-01', '2024-10-31'),
+		('Turma Marketing Digital', '2024-10-01', '2024-11-30'),
+		('Turma Gestão de Projetos', '2024-11-01', '2024-12-31'),
+		('Turma Inteligência Artificial', '2024-12-01', '2025-01-31'),
+		('Turma Redes de Computadores', '2025-01-01', '2025-02-28'),
+		('Turma Programação em Python', '2025-02-01', '2025-03-31'),
+		('Turma Banco de Dados', '2025-03-01', '2025-04-30'),
+		('Turma Segurança da Informação', '2025-04-01', '2025-05-31');
+
+	INSERT INTO resilia.turmas (nome, data_inicio, data_termino)
+	SELECT nome, data_inicio, data_termino FROM temp_turmas;
+
+	---INSERIR DADOS DA TABELA MODULO:
+
+	CREATE TEMP TABLE IF NOT EXISTS temp_modulos (
+    nome varchar,
+    descricao varchar,
+    carga_horaria int
+);
+
+	INSERT INTO temp_modulos (nome, descricao, carga_horaria)
+	VALUES
+		('Introdução ao Desenvolvimento Web', 'Fundamentos básicos de desenvolvimento web', 40),
+		('Introdução à Ciência de Dados', 'Princípios essenciais de data science', 40),
+		('Fundamentos de Design Gráfico', 'Conceitos básicos de design visual', 40),
+		('Estratégias de Marketing Online', 'Práticas de marketing digital', 40),
+		('Gerenciamento de Projetos', 'Princípios de gestão de projetos', 40),
+		('Introdução à Inteligência Artificial', 'Conceitos fundamentais de IA', 40),
+		('Fundamentos de Redes', 'Noções básicas sobre redes', 40),
+		('Python Básico', 'Introdução à programação em Python', 40),
+		('Introdução a Bancos de Dados', 'Princípios de bancos de dados', 40),
+		('Fundamentos de Segurança Cibernética', 'Noções básicas de segurança da informação', 40);
+
+	INSERT INTO resilia.Modulos (nome, descricao, carga_horaria)
+	SELECT nome, descricao, carga_horaria FROM temp_modulos;
 
 
---PROCEDURE PARA INSERIR TURMAS:
-CREATE OR REPLACE PROCEDURE resilia.inserir_turmas()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    -- Cria a tabela temporária
-    CREATE TEMP TABLE IF NOT EXISTS resilia.temp_turmas (
-        nome VARCHAR(255),
-        data_inicio DATE,
-        data_termino DATE
-    );
-
-    -- Insere os dados na tabela temporária
-    INSERT INTO resilia.temp_turmas (nome, data_inicio, data_termino)
-    VALUES 
-        ('Turma Desenvolvimento Web', '2024-07-01', '2024-08-31'),
-        ('Turma Data Science', '2024-08-01', '2024-09-30'),
-        ('Turma Design Gráfico', '2024-09-01', '2024-10-31'),
-        ('Turma Marketing Digital', '2024-10-01', '2024-11-30'),
-        ('Turma Gestão de Projetos', '2024-11-01', '2024-12-31'),
-        ('Turma Inteligência Artificial', '2024-12-01', '2025-01-31'),
-        ('Turma Redes de Computadores', '2025-01-01', '2025-02-28'),
-        ('Turma Programação em Python', '2025-02-01', '2025-03-31'),
-        ('Turma Banco de Dados', '2025-03-01', '2025-04-30'),
-        ('Turma Segurança da Informação', '2025-04-01', '2025-05-31');
-
-		    INSERT INTO resilia.turmas (nome, data_inicio, data_termino)
-        SELECT nome, data_inicio, data_termino FROM temp_turmas;
 END;
 $$;
